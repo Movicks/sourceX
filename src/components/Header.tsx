56969e8aaae433cbc00a024eecda434e40374c60
Victor <victoride013@gmail.com>
2025-08-19 18:16:54 +0100

    code corrections

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Menu, X } from 'lucide-react';
+import Image from 'next/image';
 
 // Links
 const navLinks = [
@@ -13,6 +14,20 @@ const navLinks = [
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <motion.header 
@@ -21,38 +36,50 @@ const Header = () => {
       transition={{ duration: 0.6 }}
       className="absolute z-50 w-full px-2 md:px-4 lg:px-5 lg:pt-4"
     >
-      <div className="py-3 lg:py-2 flex items-center justify-between px-2 md:px-4 w-full">
+      <div className="py-2 lg:py-2 flex items-center justify-between px-2 md:px-4 w-full fixed top-0 left-0 z-50">
         
         {/* Logo */}
         <motion.div 
-          className="flex items-center space-x-2 border-2 border-blue-900/80 rounded-tl-full rounded-br-full bg-[#000204] px-8 py-2 lg:px-13 lg:py-2"
+          className={`flex items-center space-x-2 rounded-tl-full rounded-br-full px-3 py-2 lg:px-13 
+            transition-all duration-600 ease-in-out
+            ${isScrolled ? "border-2 border-blue-900/80 bg-[#000204] px-8 lg:px-13" : "bg-transparent"}`}
           whileHover={{ scale: 1.05 }}
           transition={{ type: "spring", stiffness: 400, damping: 10 }}
         >
-          <div className="w-8 h-8 bg-white rounded-lg flex items-center justify-center">
-            <div className="w-4 h-4 bg-black rounded-sm flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-xs"></div>
-            </div>
-          </div>
-          <span className="text-2xl font-semibold text-white whitespace-nowrap">Source X</span>
+          <Image src="/icons/webLogo.jpg" width={200} height={20} alt='logo' />
         </motion.div>
 
         {/* Desktop Navigation */}
-        <nav className="hidden md:flex items-center justify-center text-lg space-x-8 border-2 border-blue-900/80 rounded-tl-full rounded-br-full bg-[#000204] lg:px-13 lg:w-full py-5 lg:py-3 text-center">
+        <nav 
+          className={`hidden md:flex items-center justify-center text-md space-x-8 rounded-tl-full rounded-br-full 
+            lg:px-13 py-5 lg:py-3 text-center 
+            transition-all duration-600 ease-in-out
+            ${isScrolled ? "border-2 border-blue-900/80 bg-[#000204]" : "bg-transparent"}`}
+        >
           {navLinks.map((link, index) => (
-            <a key={index} href={link.href} className="text-gray-300 hover:text-white transition-colors">
+            <a 
+              key={index} 
+              href={link.href} 
+              className="text-gray-300 hover:text-white transition-colors uppercase"
+            >
               {link.name}
             </a>
           ))}
         </nav>
 
         {/* Right side buttons + Mobile Menu Button */}
-        <div className="flex items-center space-x-4 border-2 border-blue-900/80 rounded-tl-full rounded-br-full bg-[#000204] lg:px-13 py-3 lg:py-1">
-          <button className="hidden md:inline bg-gray-200 text-black px-4 py-2 rounded-lg text-lg font-medium hover:bg-gray-100 whitespace-nowrap">Contact Us</button>
+        <div 
+          className={`flex items-center space-x-4 rounded-tl-full rounded-br-full px-4 lg:px-13 py-3 lg:py-1 
+            transition-all duration-600 ease-in-out
+            ${isScrolled ? "border-2 border-blue-900/80 bg-[#000204] px-8 lg:px-13" : "bg-transparent"}`}
+        >
+          <button className="hidden md:inline bg-gray-200 uppercase text-black px-4 rounded-tl-full rounded-br-full py-2 rounded-lg text-md font-medium hover:bg-gray-100 whitespace-nowrap">
+            Contact Us
+          </button>
           {/* Mobile Menu Toggler */}
           <button 
             onClick={() => setIsOpen(!isOpen)} 
-            className="md:hidden text-gray-300 hover:text-white transition-colors px-8"
+            className="md:hidden text-gray-300 hover:text-white transition-colors"
           >
             {isOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
           </button>
@@ -85,27 +112,35 @@ const Header = () => {
               }}
               className="fixed top-0 left-0 h-full w-62 bg-[#000204] border-r-2 border-blue-900/80 z-50 py-6 px-4 flex flex-col space-y-8"
             >
-            <motion.div 
-              className="flex items-center space-x-2 py-2 lg:px-13 lg:py-2"
-              whileHover={{ scale: 1.05 }}
-              transition={{ type: "spring", stiffness: 400, damping: 10 }}
-            >
-              <div className="w-8 h-8 bg-white rounded-lg flex items-center justify-center">
-                <div className="w-4 h-4 bg-black rounded-sm flex items-center justify-center">
-                  <div className="w-2 h-2 bg-white rounded-xs"></div>
-                </div>
-              </div>
-              <span className="text-2xl font-semibold text-white whitespace-nowrap">Source X</span>
+              <motion.div 
+                className="flex items-center space-x-2 py-2 lg:px-13 lg:py-2"
+                whileHover={{ scale: 1.05 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              >
+               <motion.div 
+                  className={`flex items-center space-x-2 py-2 lg:py-2  border-2 border-blue-900/80 bg-[#000204] px-8 lg:px-13 
+                    transition-all duration-600 ease-in-out`}
+                  whileHover={{ scale: 1.05 }}
+                  transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                >
+                  <Image src="/icons/webLogo.jpg" width={200} height={20} alt='logo' />
+                </motion.div>
               </motion.div>
               
               {navLinks.map((link, index) => (
-                <a key={index} href={link.href} className="block text-gray-300 hover:text-white text-lg">
+                <a 
+                  key={index} 
+                  href={link.href} 
+                  className="block text-gray-300 hover:text-white text-lg"
+                >
                   {link.name}
                 </a>
               ))}
               
               <div className="flex flex-col justify-end space-y-3 pt-4 border-t-2 border-blue-900/80 h-full pb-6">
-                <button className="inline md:hidden bg-white h-[2.8rem] text-lg text-black px-4 rounded-lg font-medium hover:bg-gray-100 rounded-tl-full rounded-br-full">Contact Us</button>
+                <button className="inline md:hidden bg-white h-[2.8rem] text-lg text-black px-4 rounded-lg font-medium hover:bg-gray-100 rounded-tl-full rounded-br-full">
+                  Contact Us
+                </button>
               </div>
             </motion.div>
           </>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -169,7 +169,7 @@ const HeroSection = () => {
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 0.3 }}
-          className="text-2xl md:text-4xl lg:text-6xl font-bold mb-3 md:mb-6 leading-tight mt-30 lg:mt-50 text-center md:text-start"
+          className="text-2xl md:text-4xl lg:text-[3.5rem] font-bold mb-5 md:mb-6 leading-tight mt-30 md:mt-50 lg:mt-40 xl:mt-35 text-center md:text-start"
         >
           <span className="block bg-gradient-to-r from-blue-500 via-blue-100 to-white bg-clip-text text-transparent">
             The AI Transformation Partner<br className='hidden lg:block'/> Your Competition Fears
@@ -180,9 +180,18 @@ const HeroSection = () => {
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 0.5 }}
-          className="text-lg md:text-xl text-gray-300 mb-3 md:mb-6 leading-relaxed text-center md:text-start"
+          className="text-lg md:text-xl text-gray-400 mb-5 md:mb-6 leading-relaxed text-center md:text-start"
         >
-          <span className='max-w-[30rem] whitespace-break-spaces lg:text-2xl'>We audit your workflows, build custom AI solutions, and deliver <br className='hidden lg:block'/>measurable ROI - making you the most efficient and profitable <br className='hidden lg:block'/>version of your business.</span>
+          <span className='max-w-[30rem] whitespace-break-spaces lg:text-[1.4rem]'>We audit your workflows, build custom AI solutions, and deliver <br className='hidden lg:block'/>measurable ROI - making you the most efficient and profitable <br className='hidden lg:block'/>version of your business.</span>
+        </motion.p>
+
+        <motion.p 
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          className="text-lg md:text-xl text-gray-400 mb-5 md:mb-6 leading-relaxed text-center md:text-start"
+        >
+          <span className='max-w-[30rem] whitespace-break-spaces lg:text-[1.4rem]'>“Not just another AI automation agency. We are your competitive advantage.”</span>
         </motion.p>
         
         <motion.div 
@@ -192,18 +201,18 @@ const HeroSection = () => {
           className="flex flex-col sm:flex-row gap-3 items-center"
         >
           <motion.button whileTap={{ scale: 0.95 }} onMouseMove={ handleMouseMove } className="relative w-full lg:max-w-[18rem] h-[3rem] flex items-center justify-center text-black gap-8 overflow-hidden rounded-tl-full rounded-br-full px-4 py-1 bg-gray-200 font-semibold transition-all duration-300 shadow-md hover:shadow-lg">
-            <span className="md:text-black">See Results</span>
+            <span className="md:text-black">SEE RESULTS</span>
             <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-transform md:text-black" />
           </motion.button>
           <motion.button whileTap={{ scale: 0.95 }} onMouseMove={ handleMouseMove } className="relative w-full lg:max-w-[18rem] h-[3.1rem] flex items-center justify-center bg-gray-200 text-black md:bg-[#000204] md:border-2 border-blue-900/80 md:text-white gap-8 overflow-hidden rounded-tl-full rounded-br-full px-4 py-3 font-semibold transition-all duration-300 shadow-md hover:shadow-lg">
-            <span className="md:text-white">Get My Free AI Audit</span>
+            <span className="md:text-white uppercase">Get My Free AI Audit</span>
             <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-transform md:text-white" />
           </motion.button>
         </motion.div>
         
         {/* Visual Montage */}
         <div
-          className="mt-20 relative"
+          className="xl:mt-20 xl:mb-10 relative hidden xl:block"
         >
           <div className="flex justify-end items-start xl:-mt-12 bg-transparent max-w-[55rem] xl:min-w-[63.3rem] xl:max-w-[63.3rem] rounded-xl">
           <DashboardPreview />
