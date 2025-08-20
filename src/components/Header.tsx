684fdd3534b98b94d8503cf78bc466dd0fe68d33
Victor <victoride013@gmail.com>
2025-08-20 07:22:38 +0100

    More changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,13 +40,13 @@ const Header = () => {
         
         {/* Logo */}
         <motion.div 
-          className={`flex items-center space-x-2 rounded-tl-full rounded-br-full px-3 py-2 lg:px-13 
+          className={`flex items-center space-x-2 rounded-tl-full rounded-br-full px-3 py-2 lg:px-13 lg:py-1 
             transition-all duration-600 ease-in-out
             ${isScrolled ? "border-2 border-blue-900/80 bg-[#000204] lg:px-13 overflow-hidden" : "border-2 border-blue-900/80 lg:border-none overflow-hidden bg-[#000204] lg:bg-transparent"}`}
           whileHover={{ scale: 1.05 }}
           transition={{ type: "spring", stiffness: 400, damping: 10 }}
         >
-          <Image src="/icons/webLogo.jpg" width={200} height={20} alt='logo' className='max-w-[10rem] lg:min-w-[15rem]' />
+          <Image src="/icons/weblogo.jpg" width={200} height={20} alt='logo' className='max-w-[10rem] lg:min-w-[15rem]' />
         </motion.div>
 
         {/* Desktop Navigation */}
