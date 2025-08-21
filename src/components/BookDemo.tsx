6841020dce6a5eb5c08b69be2cf4c1c59e953cf5
Victor <victoride013@gmail.com>
2025-08-21 20:47:00 +0100

    Change of color thems

--- src/components/BookDemo.tsx
+++ src/components/BookDemo.tsx
@@ -14,7 +14,7 @@ function BookDemoGridItem({data}:{data:{
 }
 export default function BookDemo() {
   return (
-    <section className="w-full relative isolate px-peers md:px-container pb-container flex justify-center before:h-1/2 before:absolute before:bg-primary before:w-full before:left-0 before:bottom-0 before:-z-1">
+    <section className="w-full relative isolate px-peers md:px-container pb-container flex justify-center before:h-1/2 before:absolute before:bg-gradient-to-r before:from-indigo-600 before:via-sky-600 before:to-emerald-600 before:w-full before:left-0 before:bottom-0 before:-z-1">
       <main className="w-full max-w-6xl bg-accent isolate shadow-md shadow-background/30 p-sections sm:p-container md:p-sections rounded-xl grid gap-peers grid-cols-1 sm:grid-cols-[1fr_1fr] lg:grid-cols-[3fr_2fr] items-center">
         {/* content */}
         <div className="w-full flex flex-col gap-peers">
